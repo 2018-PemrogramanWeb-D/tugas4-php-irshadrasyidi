479c56110299f7bf80322703af072aaaf229587f
irshadrasyidi <didot.brodot@yahoo.com>
2018-09-29 17:06:19 +0700

    tambah form

--- login.php
+++ login.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Login Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    
+    <form>
+        Username:<br>
+        <input type="text" name="username"><br>
+        Password:<br>
+        <input type="text" name="password"><br>
+    </form>
 </body>
 </html>
